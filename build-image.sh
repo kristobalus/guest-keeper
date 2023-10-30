45022d8201721b1955e57ef0ada5ce8b6f63f49a
kristobalus <ildar.tamendarov@gmail.com>
2023-10-30 13:09:26 +0600

    update

---
+++ build-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+IMAGE=kristobalus/guest-keeper:1.0.0
+echo "building image $IMAGE..."
+
+docker build -t "$IMAGE" -f ./Dockerfile \
+		--progress=plain \
+		--label "build-tag=build-artifact" \
+		--platform=linux/amd64  . || { echo "failed to build docker image"; exit 1; }
+
+echo "pushing image $IMAGE to registry.."
+docker push "$IMAGE"
+docker image prune -f --filter label=build-tag=build-artifact
